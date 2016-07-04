4e7a0f0eda043bd2d8187c3d1aa4afaa8294785e
Nahuel Ortelli <nortelli@iphoideas.com>
2016-07-04 10:29:54 -0300

    Seguir con 'Persist Data'

--- FoodTracker/FoodTracker/MealViewController.swift
+++ FoodTracker/FoodTracker/MealViewController.swift
@@ -29,6 +29,14 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         // Asignamos esta misma clase como delegada del objeto nameTextField
         nameTextField.delegate = self
         
+        // Set up views if editing an existing Meal.
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text   = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
+        
         // Enable the Save button only if the text field has a valid Meal name.
         checkValidMealName()
     }
@@ -77,7 +85,15 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
 // MARK: Navigation
     
     @IBAction func cancel(sender: UIBarButtonItem) {
-        dismissViewControllerAnimated(true, completion: nil)
+        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        
+        if isPresentingInAddMealMode {
+            dismissViewControllerAnimated(true, completion: nil)
+        }
+        else {
+            navigationController!.popViewControllerAnimated(true)
+        }
     }
     // This method lets you configure a view controller before it's presented.
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
